af5737618eb40f3ad27c3f5b359b80e0b455e300
Jonathan Hosein <jphosein42@students.tntech.edu>
2020-04-17 20:46:07 -0500

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,37 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about-us",
-    name: "about",
+    path: "/event",
+    name: "event-show",
+    component: EventShow
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    //component: () =>
+    //import(/* webpackChunkName: "about" */ "../views/EventShow.vue")
+
     //Alternate for alias routing
     //alias: "/about"
   },
   {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
     //alias for redirect
-    path: "/about",
-    redirect: { name: "about" }
+    //path: "/about",
+    //redirect: { name: "about" }
   }
 ];
 
