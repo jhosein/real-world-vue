afab55460904780c82e757be2a694872583b253b
Jonathan Hosein <jphosein42@students.tntech.edu>
2020-04-19 13:25:14 -0500

    axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,36 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//Neccessary for Auto Global Registration
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+//Begin Auto Global Registration
+
+//Webpack feature
+//1. directory to look ing
+//2. look in sub-directories?
+//3. looking for components that start with Base and end with .vue or .js
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+//Aray of each file picked up above
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
+//End Auto Global Registration
+
+//Registers component as a global component. This eliminates the need for local component registration
+//Vue.component("BaseIcon", BaseIcon);
 
 Vue.config.productionTip = false;
 
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard></EventCard>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
+    <BaseIcon></BaseIcon>
   </div>
 </template>
 <script>
 import EventCard from "@/components/EventCard.vue";
+import axios from "axios";
 export default {
   //Import nested components
   components: {
     //Shorthand for EventCard: EventCard
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/events")
+      .then(response => {
+        console.log(response.data);
+        this.events = response.data;
+      })
+      .catch(err => {
+        console.log("There was an error: " + err);
+      });
   }
 };
 </script>
